aa51d75ab39e9df5e70b2e432c6a8b5e786a75d8
LuisMSouza <l.miguel-souza@hotmail.com>
2022-10-20 19:37:02 -0300

    commit new files

--- src/archives/1/main.c
+++ src/archives/1/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <locale.h>
 
 void Function1();
 
 int main()
 {
+    setlocale(LC_ALL, "portuguese");
     Function1();
     return 0;
 }
 
 void Function1()
 {
-
+    
 }
