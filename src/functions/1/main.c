ee929fe609af74ae140c3226254bb544216dc6da
LuisMSouza <l.miguel-souza@hotmail.com>
2022-10-24 20:07:01 -0300

    commit new files

--- src/functions/1/main.c
+++ src/functions/1/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <locale.h>
 
 int main()
 {
+    int num = 0;
     setlocale(LC_ALL, "portuguese");
+    printf("Insira um número: ");
+    scanf("%i", &num);
+    printf("O valor da soma de 1 até %i é igual a: %i", num, Somat(num));
     return 0;
+}
+
+int Somat(int number)
+{
+    int soma = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        soma += i;
+    }
+    return soma;
 }
